f1e3459976333b4a7414e875351afaea221a2204
edwinwells <edwin.wallace@yandex.com>
2017-03-27 19:34:40 -0400

    Notes for using WordPress
    from AITP meet-up 3.27.17

---
+++ WordPress_Build_A_Website.rb
@@ -0,0 +1,29 @@
+#WordPress (WP), a web-cased toolnthat can be used to build websites of all types.
+
+#WP facts:
+#PHP with some CSS files;
+#Blog and/or Static;
+#Self-sensing, mobile friendly
+#Theme-driven
+#Customization: plug-ins, modify PHP or CSS files
+# 40% of all websites are WordPress driven
+
+#The theme will determine the entire presentation's morphsing... scroll text/change photo, etc.
+
+#1) Choose theme;
+#2) Give site an address;
+#3) Add image(s) either from their selection, or upload;
+#3a) You can select several images, and each refresh
+#    will randomize to post a new image from selection
+
+
+# ) Save & publish
+
+# Goto mysite/customize (must have admin. access)
+# then, edit page, add new pages (appearance/menus (primary menu checkbox)), etc. 
+#Blog posts
+
+#** Remember- posts will be displayed in reversed chronological order! **
+
+#Settings/Reading: gives the option yo switch from blog to static, save changes & return to/ refresh page; blog posts will be in the menu, under their own category.
+
