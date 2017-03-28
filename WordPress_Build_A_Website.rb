0c806f157c7265ad8dcf0fc0ceb526affbec3ebe
edwinwells <edwin.wallace@yandex.com>
2017-03-28 00:23:32 -0400

    Removed typos

--- WordPress_Build_A_Website.rb
+++ WordPress_Build_A_Website.rb
@@ -1,4 +1,4 @@
-#WordPress (WP), a web-cased toolnthat can be used to build websites of all types.
+#WordPress (WP), a web-cased tool that can be used to build websites of all types.
 
 #WP facts:
 #PHP with some CSS files;
@@ -8,7 +8,7 @@
 #Customization: plug-ins, modify PHP or CSS files
 # 40% of all websites are WordPress driven
 
-#The theme will determine the entire presentation's morphsing... scroll text/change photo, etc.
+#The theme will determine the entire presentation's morphing... scroll text/change photo, etc.
 
 #1) Choose theme;
 #2) Give site an address;
@@ -17,7 +17,7 @@
 #    will randomize to post a new image from selection
 
 
-# ) Save & publish
+#4) Save & publish
 
 # Goto mysite/customize (must have admin. access)
 # then, edit page, add new pages (appearance/menus (primary menu checkbox)), etc. 
@@ -26,4 +26,3 @@
 #** Remember- posts will be displayed in reversed chronological order! **
 
 #Settings/Reading: gives the option yo switch from blog to static, save changes & return to/ refresh page; blog posts will be in the menu, under their own category.
-
